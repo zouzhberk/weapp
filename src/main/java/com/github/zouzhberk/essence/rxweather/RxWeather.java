328ae9b06425e14a6c99dfb083aaa2f204666cf5
berk <zouzhberk@gmail.com>
2016-03-26 22:28:28 +0800

    update custom configuration.

--- src/main/java/com/github/zouzhberk/essence/rxweather/RxWeather.java
+++ src/main/java/com/github/zouzhberk/essence/rxweather/RxWeather.java
@@ -1,8 +1,21 @@
 package com.github.zouzhberk.essence.rxweather;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 
 /**
  * Created by berk (zouzhberk@163.com)) on 3/23/16.
@@ -28,8 +41,34 @@ public class RxWeather {
         }
     }
 
+    public OkHttpClient getSSLHttpClient() {
+
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            InputStream instream = getClass().getResourceAsStream("api.heweather.com.crt");
+            Certificate ca;
+            ca = cf.generateCertificate(instream);
+            KeyStore kStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            kStore.load(null, null);
+            kStore.setCertificateEntry("ca", ca);
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(kStore);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), null);
+            okHttpClientBuilder.sslSocketFactory(sslContext.getSocketFactory());
+        } catch (CertificateException
+                | KeyStoreException
+                | NoSuchAlgorithmException
+                | IOException
+                | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return okHttpClientBuilder.build();
+    }
+
     private RxWeather() {
-        retrofit = new Retrofit.Builder().addConverterFactory
+        retrofit = new Retrofit.Builder().client(getSSLHttpClient()).addConverterFactory
                 (JacksonConverterFactory
                         .create())
                 .baseUrl(baseUrl == null ? "http://api.heweather.com/" : null)
